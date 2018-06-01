912c7717ea7dca6f708dea439e85378e774d55b8
mwang.arcsoft <mwang@arcsoft.com.cn>
2018-06-01 19:05:16 +0800

    update for tempfs env setup

--- myenv/bin/copy_env.sh
+++ myenv/bin/copy_env.sh
@@ -1,35 +1,45 @@
-sudo cp -r .bin       /home/ml
-sudo cp -r .vim*      /home/ml
-sudo cp -r .tmux*     /home/ml
-sudo cp .bash*        /home/ml
-sudo cp .bash*        /home/ml
-sudo cp .profile      /home/ml
-sudo cp .commacd.bash /home/ml
-sudo cp .fzf.*        /home/ml
-sudo cp .git*         /home/ml
-sudo chown -R ml:ml   /home/ml/.*
-sudo rsync -rtopgDzv /usr/bin/* /home/ml/.bin/
-sudo rsync -rtopgDzv /bin/* /home/ml/.bin/
+envpath='/home/lma'
+homepath='/home/ml'
+binpath='.env/usr/bin'
+libpath='.env/usr/lib'
 
-sudo cp /usr/bin/vim.basic /home/ml/.bin/vim
-sudo cp /usr/bin/gawk /home/ml/.bin/awk
-sudo cp /usr/lib/android-sdk/platform-tools/adb /home/ml/.bin/adb
-sudo cp /usr/lib/android-sdk/platform-tools/fastboot /home/ml/.bin/fastboot
-sudo ln -s /home/ml/.bin/x86_64-linux-gnu-g++-7 /home/ml/.bin/g++
-sudo ln -s /home/ml/.bin/x86_64-linux-gnu-gcc-7 /home/ml/.bin/gcc
-sudo ln -s /home/ml/.bin/x86_64-linux-gnu-cpp-7 /home/ml/.bin/cpp
-sudo ln -s /home/ml/.bin/x86_64-linux-gnu-readelf /home/ml/.bin/readelf
-sudo ln -s /home/ml/.bin/x86_64-linux-gnu-objdump /home/ml/.bin/objdump
-sudo ln -s /home/ml/.bin/x86_64-linux-gnu-strings /home/ml/.bin/strings
-sudo ln -s /home/ml/.bin/x86_64-linux-gnu-c++filt /home/ml/.bin/c++filt
-sudo ln -s /home/ml/.bin/x86_64-linux-gnu-addr2line /home/ml/.bin/addr2line
-sudo ln -s /home/ml/.bin/x86_64-linux-gnu-gcc-ranlib-7 /home/ml/.bin/gcc-ranlib-7
-sudo ln -s /home/ml/.bin/x86_64-linux-gnu-nm /home/ml/.bin/nm
-sudo ln -s /home/ml/.bin/ctags-exuberant /home/ml/.bin/ctags
-sudo ln -s /home/ml/.bin/convert-im6.q16 /home/ml/.bin/convert
-sudo ln -s /home/ml/.bin/identify-im6.q16 /home/ml/.bin/identify
+sudo cp -r $envpath/.bin       $homepath
+sudo cp -r $envpath/.vim*      $homepath
+sudo cp -r $envpath/.tmux*     $homepath
+sudo cp $envpath/.bash*        $homepath
+sudo cp $envpath/.bash*        $homepath
+sudo cp $envpath/.profile      $homepath
+sudo cp $envpath/.commacd.bash $homepath
+sudo cp $envpath/.fzf.*        $homepath
+sudo cp $envpath/.git*         $homepath
 
-sudo mkdir /home/ml/.lib
-sudo rsync -rtopgDzvl /lib/x86_64-linux-gnu/* /home/ml/.lib/
-sudo chmod a+w /home/ml/.bashrc
-echo 'export LD_LIBRARY_PATH=.:/home/ml/.lib:$LD_LIBRARY_PATH:/usr/lib/x86_64-linux-gnu:/usr/local/lib/:/usr/local/lib64/' >> /home/ml/.bashrc
+sudo chown -R ml:ml   $homepath
+
+sudo mkdir -p $homepath/$binpath
+sudo mkdir -p $homepath/$libpath
+
+sudo rsync -rtopgDzv /usr/bin/* $homepath/$binpath/
+sudo rsync -rtopgDzv /bin/* $homepath/$binpath/
+sudo rsync -rtopgDzvl /lib/x86_64-linux-gnu/* $homepath/$libpath/
+sudo rsync -rtopgDzvl /usr/lib/gcc $homepath/$libpath/
+
+sudo cp /usr/bin/vim.basic $homepath/$binpath/vim
+sudo cp /usr/bin/gawk $homepath/$binpath/awk
+sudo cp /usr/lib/android-sdk/platform-tools/adb $homepath/$binpath/adb
+sudo cp /usr/lib/android-sdk/platform-tools/fastboot $homepath/$binpath/fastboot
+sudo ln -s $homepath/$binpath/x86_64-linux-gnu-g++-7 $homepath/$binpath/g++
+sudo ln -s $homepath/$binpath/x86_64-linux-gnu-gcc-7 $homepath/$binpath/gcc
+sudo ln -s $homepath/$binpath/x86_64-linux-gnu-cpp-7 $homepath/$binpath/cpp
+sudo ln -s $homepath/$binpath/x86_64-linux-gnu-readelf $homepath/$binpath/readelf
+sudo ln -s $homepath/$binpath/x86_64-linux-gnu-objdump $homepath/$binpath/objdump
+sudo ln -s $homepath/$binpath/x86_64-linux-gnu-strings $homepath/$binpath/strings
+sudo ln -s $homepath/$binpath/x86_64-linux-gnu-c++filt $homepath/$binpath/c++filt
+sudo ln -s $homepath/$binpath/x86_64-linux-gnu-addr2line $homepath/$binpath/addr2line
+sudo ln -s $homepath/$binpath/x86_64-linux-gnu-gcc-ranlib-7 $homepath/$binpath/gcc-ranlib-7
+sudo ln -s $homepath/$binpath/x86_64-linux-gnu-nm $homepath/$binpath/nm
+sudo ln -s $homepath/$binpath/ctags-exuberant $homepath/$binpath/ctags
+sudo ln -s $homepath/$binpath/convert-im6.q16 $homepath/$binpath/convert
+sudo ln -s $homepath/$binpath/identify-im6.q16 $homepath/$binpath/identify
+
+sudo chmod a+w $homepath/.bashrc
+echo "export LD_LIBRARY_PATH=.:$homepath/$libpath:$LD_LIBRARY_PATH:/usr/lib/x86_64-linux-gnu:/usr/local/lib/:/usr/local/lib64/" >> $homepath/.bashrc
