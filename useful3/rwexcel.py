37a129e0684e9f9a9802bd3a4471582511c81150
lma <starpicker1@163.com>
2020-06-12 10:27:04 +0800

    update

--- useful3/rwexcel.py
+++ useful3/rwexcel.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from openpyxl import Workbook, load_workbook
 from openpyxl.styles import Font, Alignment
 from openpyxl.utils import get_column_letter
@@ -18,22 +19,20 @@ if len(wm)>0 :
         cell2 = str(wm[i]).replace('(<CellRange ','').replace('>,)','')
         mergeArr.append(cell2)
         #wbsheet_new.merge_cells(cell2)
-# print(mergeArr)
+#print(mergeArr)
         
-counts = 500
+counts = 15
 row_start = 1
 col_start = 1
-row_ends = 10
+row_ends = 29
 col_ends = 12
 row_span = row_ends-row_start+1
-modi_value = 202000027
+modi_value = 202000220
 string = '合肥惠丰电子科技有限公司   IT末梢终端耗材、零配件出库单  NO:YS03 '
 
 for n in range(counts):
-        
-    modi_value += 1
     
-    for rows in range(row_start, row_ends):
+    for rows in range(row_start, row_ends+1):
         wbsheet_new.row_dimensions[rows+n*row_span].height = wbsheet.row_dimensions[rows].height
         
         for cols in range(col_start, col_ends):
@@ -41,6 +40,13 @@ for n in range(counts):
             target_cell = wbsheet_new.cell(row=rows+n*row_span, column=cols, value = wbsheet.cell(rows, cols).value)
             if (rows == row_start and cols == col_start):
                 target_cell = wbsheet_new.cell(row=rows+n*row_span, column=cols, value = string + str(modi_value))
+                modi_value += 1
+            if (rows == row_start+10 and cols == col_start):
+                target_cell = wbsheet_new.cell(row=rows+n*row_span, column=cols, value = string + str(modi_value))
+                modi_value += 1
+            if (rows == row_start+20 and cols == col_start):
+                target_cell = wbsheet_new.cell(row=rows+n*row_span, column=cols, value = string + str(modi_value))
+                modi_value += 1
                 
             source_cell = wbsheet.cell(rows, cols)
 
@@ -61,7 +67,9 @@ for n in range(counts):
     
     for i in mergeArr:
         temp1 = findall(r'[A-Z]', i)
-        temp2 = findall(r'[0-9]', i)
+        temp2 = findall(r'\d+', i)
+        #print(temp1)
+        #print(temp2)
         temp3 = [str(int(temp2[0])+n*row_span), str(int(temp2[1])+n*row_span)]
         temp4 = temp1[0]+temp3[0] + ":" + temp1[1]+temp3[1]
         wbsheet_new.merge_cells(temp4)
